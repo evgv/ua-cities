e09e4a442b81b22191e93cb484580a295ebf6dfd
evgv <evgvzubkoff@gmail.com>
2017-10-18 12:23:01 +0300

    New features

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ua-cities
+# Small static web-site with short info about Ukraine cities.     
+
+- cities info load from `json.file`
+- used [skeleton]:http://getskeleton.com/
+- hosted on [heroku]:https://heroku.com/ like a static web site
--- public/js/app.js
+++ public/js/app.js
@@ -1,53 +1,68 @@
-function loadJSON(callback) {
+/**
+ * App class
+ */
 
-    var xobj = new XMLHttpRequest();
-    
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'cities.json', true);
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-        }
-    };
-    
-    xobj.send(null);
+if (typeof module == 'undefined') {
+    var module = {};
 }
 
-function init() {
-    loadJSON(function (response) {
-        var json = JSON.parse(response);
+module.App = function () {
+    
+    this.jsonFile = 'cities.json';
+    
+    this.loadJSON = function(callback) {
+        
+        var xobj = new XMLHttpRequest();
 
-        document.getElementById("city").addEventListener('keyup', search);
-
-        var resultWrapper = document.getElementById("search-result");
-
-        function search() {
-
-            var query = this.value;
-
-            if (query.length) {
-                resultWrapper.innerHTML = '';
-
-                json.cities.forEach(function(element) {
-                    var pattern = new RegExp('^' + query, 'i');
-                    var result  = pattern.test(element.name);
-
-                    if (result === true) {
-
-                        var div = document.createElement('div');
-                            div.innerHTML = '<li class="city-wrap"><a href="//google.com/search?q=' + element.name + '" target="_blank" class="city-name"><b>' + element.name + '</b></a> <p>In ' + element.subdivision + ', population is '+ element.population + ', status "' + element.status + '"</p></li>';
-
-                        while (div.children.length > 0) {
-                            resultWrapper.appendChild(div.children[0]);
-                        }
-                    }
-                });
-            } else {
-                resultWrapper.innerHTML = '';
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', this.jsonFile, true);
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                callback(xobj.responseText);
             }
+        };
 
-        }
-    });
-}
+        xobj.send(null);
+    };
+};
 
-init();
+module.App.prototype = {
+    
+   init: function() {
+        this.loadJSON(function (response) {
+            
+            var json = JSON.parse(response),
+                resultWrapper = document.getElementById("search-result");
+            
+            document.getElementById("city").addEventListener('keyup', function() {
+                var query = this.value;
+                if (query.length) {
+                    resultWrapper.innerHTML = '';
+
+                    json.cities.forEach(function(element) {
+                        var pattern = new RegExp('^' + query, 'i');
+                        var result  = pattern.test(element.name);
+
+                        if (result === true) {
+
+                            var div = document.createElement('div');
+                                div.innerHTML = '<li class="city-wrap"><a href="//google.com/search?q=' + element.name + '" target="_blank" class="city-name"><b>' + element.name + '</b></a> <p>In ' + element.subdivision + ', population is '+ element.population + '"</p></li>';
+
+                            while (div.children.length > 0) {
+                                resultWrapper.appendChild(div.children[0]);
+                            }
+                        }
+                    });
+                } else {
+                    resultWrapper.innerHTML = '';
+                }
+            });
+        });
+    }
+};
+
+var app = new module.App();
+
+window.onload = function() {
+    app.init();
+};
